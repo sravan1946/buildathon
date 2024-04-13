4949776d855e9dcec4dda03eb99ab180f9e8828c
sravan1946 <miniclip1956@gmail.com>
2024-04-13 06:13:04 +0530

    Add dashboard route to app.py

--- app.py
+++ app.py
@@ -135,3 +135,7 @@ def login():
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
